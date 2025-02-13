5fe876e5f827a46ccce4f29a565925c9763a4782
Animish2002 <animishchopade123@gmail.com>
2025-02-13 15:58:12 +0530

    project initialize

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <div className="text-red-600 text-2xl text-center">
+        checking the tailwind working or not !!
+      </div>
+    </>
+  );
+}
+
+export default App;
